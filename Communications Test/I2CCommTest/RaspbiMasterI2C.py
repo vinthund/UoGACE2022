ffaf99151b8946706f5986a2bee02d8df85f6b10
JoelSmalls <joelokanta46@gmail.com>
2022-02-25 10:33:06 +0000

    [Update] Version 1.1 of Python I2C Code, deletion of other experimental files

--- Communications Test/I2CCommTest/RaspbiMasterI2C.py
+++ Communications Test/I2CCommTest/RaspbiMasterI2C.py
@@ -1,5 +1,26 @@
 from smbus import SMBus
 
+clientAddr = 0x08
+bus = SMBus(1)
+
+def i2cWrite(msg):
+  for c in msg:
+    bus.write_byte(clientAddr, ord(c))
+  return -1
+
+def main():
+  print("Send message to Arduino")
+  while True:
+    msg = input("$> ")
+    print("...")
+    i2cWrite(msg)
+
+if __name__ == "__main__":
+  main()
+
+
+"""from smbus import SMBus
+
 addr = 0x8 # bus address
 bus = SMBus(1) # indicates /dev/ic2-1
 
@@ -16,3 +37,4 @@ while numb == 1:
         bus.write_byte(addr, 0x0) # switch it on
     else:
         numb = 0
+"""
