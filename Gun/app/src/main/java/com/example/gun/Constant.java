eb9af33227434dbb6adefd7e6a52f1ecfdb65a7b
kamyar123 <kamyar658@gmail.com>
2022-02-27 02:49:30 +0000

    merge

---
+++ Gun/app/src/main/java/com/example/gun/Constant.java
@@ -0,0 +1,9 @@
+package com.example.gun;
+
+public class Constant {
+    public static String invalidMailMessage = "Mail is invalid!";
+    public static String mailExistsMessage = "Mail and password required!";
+    public static String accountNotExistsMessage = "Mail and password required!";
+    public static String registerSuccessfulMessage = "User Registered Successfully";
+    public static String loggedInSuccessfulMessage = "User Logged in Successfully";
+}
