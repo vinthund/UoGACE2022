2ca94fe83c78a290dde56dbb914b71c010c6c733
JoelSmalls <joelokanta46@gmail.com>
2022-03-03 14:10:39 +0000

    [UPDATE n DEPRECATION] Version 1.5.1 of RaspberryPi Master COde, modifications mande including addition of new function StatusCodes, removal of encoding variable and additional changes to allow for smoother transmission of data to Arduino. SerialUART has been deprecated as it is no longer in use.

--- Communications Test/I2CCommTest/RaspbiMasterI2C.py
+++ Communications Test/I2CCommTest/RaspbiMasterI2C.py
@@ -8,15 +8,17 @@
 * Although the code could be utilised to send characters instead of numbers, it is overall better to use numbers so as to reduce bandwidth and keep
 * communication speed high.
 *
-* Version 1.4.0
+* Version 1.5.1
+* Recent Amendments: Removal of encoding variable, amendment made to statusCode function, statusCodes does not work as of yet.
+* Status Codes function works similar to API Status calls, so instead of sending words over I2C, numbers are instead sent and these numbers are given conditions. 
 """
 
 import time
 from smbus import SMBus
 
+
 clientAddr = 0x08
 bus = SMBus(1)
-encoding = 'utf-8'
 
 def i2cWrite(msg):
   for c in msg:
@@ -24,17 +26,40 @@ def i2cWrite(msg):
   return -1
 
 def i2cRead():
-    for i in range(0, 20):
+    while True:
         data = bus.read_byte(clientAddr)
-    print("From Arduino: %02d" % data)
+        #print("From Arduino: %02d" % data)
+        time.sleep(1)
+        
+def statusCodes():
+    n = i2cRead()
+    if n == 1:
+        print("Arduino: Connection Established")
+    elif n == 2:
+        print("Arduino: Data Received!")
+    elif n == 21:
+        print("Arduino: Out of Ammo!")
+    elif n == 22:
+        print("Shooting Error! Fix Immediately!")
+    elif n == "100":
+        print("Arduino: Currently Alive!")
+    elif n == 110:
+        print("Arduino: Program Error!")
+    elif n == 115:
+        print("Arduino: Mechanical Error!")
+    else:
+        print("Arduino Status Unknown")
+    
 
 def main():
   print("Send message to Arduino")
   while True:
+    #print(data)
     msg = input("> ")
     print("...")
     i2cWrite(msg)
-    i2cRead()
+    #i2cRead()
+    #statusCodes()
     
 
 if __name__ == "__main__":
@@ -42,3 +67,4 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print("Program Terminated Manually")
+ 
