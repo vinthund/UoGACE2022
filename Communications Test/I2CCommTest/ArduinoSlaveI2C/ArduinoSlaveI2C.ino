013e5180d79da8ebcb10f9821904e5468a18546b
JoelSmalls <joelokanta46@gmail.com>
2022-03-11 11:24:27 +0000

    [UPDATE] Version 2.3.0 of Arduino Code. New modifications feature removal of startup function and moving its contents to "void setup()", addition of static variables to store old versions of incoming values, fixed bug of servo not moving due to having extra "=" on boolean assignment and removal of redundant variables.

--- Communications Test/I2CCommTest/ArduinoSlaveI2C/ArduinoSlaveI2C.ino
+++ Communications Test/I2CCommTest/ArduinoSlaveI2C/ArduinoSlaveI2C.ino
@@ -5,10 +5,12 @@
     SCL <--> SCL
     GND <--> GND
 
-    Version 2.1.9
+    Version 2.3.0
    Changes made:
-   * Removal of startup function, simplifying it into just "toSend=1" on "void setup()"
-   * Addition of static bytes to store old values of positions received.
+     * Removal of startup function, simplifying it into just "toSend=1" on "void setup()"
+     * Addition of static int to store old values of positions received. This is so that the servo's position does not reset every time it receives coordinates.
+     * Fixed Bug of Servo not moving. Main cause of issue was due to having another "=" on variable assignment. :/
+     * Removal of redundant variables.
 */
 
 // Include the Wire library for I2C
@@ -16,25 +18,27 @@
 #include <Servo.h>
 
 Servo myservo;
-// Definition of H Bridge, LED pins and integers, note that these are here for test purposes and may be controlled using I2C.
+Servo myservo2;
 String received_str = "";
-int toSend = 1;
-int x, y, ammo, mapper;
-int pos, ypos = 0;
-bool flag, constant, trigger, movement = false;
+int toSend;
+int x, y, ammo;
+bool flag, trigger, movement = false;
 
 void setup() {
   myservo.attach(2);
+  myservo2.attach(3);
   // Join I2C bus as slave with address 8
   Wire.begin(0x8);
-  toSend = 1;
-  delay(3000);
-  flag = true;
+
   // Call receiveEvent when data received and requestEvent when data is to be sent
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
 
   Serial.begin(9600);
+  //Connection establishment: Sets "toSend" to 1 which means "Connection Established". This then sets the flag to true so that it does not set "toSend" to 1 again.
+  toSend = 1;
+  delay(3000);
+  flag = true;
 }
 
 // Function that executes whenever data is received from master
@@ -52,8 +56,11 @@ void requestEvent() {
 
 // Function that deals with status codes to control Arduino
 void statusCodes() {
-  static byte x_old = 0;
-  static byte y_old = 0;
+  //Static keyword used to create variable for only one function. This, unlike other variables, keeps its value after function calls, allowing it to
+  // be compared with newer versions of values to make comparisons with them. This is used in this code to avoid the servo moving over to the same position again
+  // even though it is receiving the same coordinates.
+  static int x_old = 0;
+  static int y_old = 0;
 
   if (received_str == "1") {
     Serial.println("Raspberry Pi - Connection Established");
@@ -63,30 +70,28 @@ void statusCodes() {
     Serial.println("Raspberry Pi - Status OK");
   } else if (received_str[0] == '(' ) {
     Serial.println("Coordinates Received, Ready to Delete Target!");
+    //sscanf is used here to read formatted input from a string, then assigns the values to be read onto integer varianbles.
+    // in this case, they are set to coordinate "x" and "y" variables. 
     int r = sscanf(received_str.c_str(), "(%d, %d)", &x, &y);
     Serial.print("X: ");
     Serial.println(x);
     Serial.print("Y: ");
     Serial.println(y);
+    // Below conditional statement checks if the current coordinate variables are equal to its previous versions stored on the static int variables.
+    // In the case it is, the movement flag is set to false, blocking movement from occuring as no change has occured.
+    // However, in the case that it is not the same as its previous version, movement is set to true, then the coordinate variables are stored in the static int variables,
+    // so as to be compared to the next values sent.
     if (x == x_old && y == y_old) {
       movement = false;
     } else {
-      movement == true;
+      movement = true;
       x_old = x;
       y_old = y;
     }
   }
 }
-/*
-// Function that deals with startUp, establishing connection between the Pi and the Arduino.
-void startUp() {
-  if (flag == false) {
-    toSend = 1;
-    delay(3000); // delay of 3 seconds before setting flag to true
-    flag = true;
-  }
-}
-*/
+
+//The Below function works on blocking strings/characters from appearing except brackets and numbers.
 void convertToInt() {
   int n = received_str.toInt();
   if (n > 0 || received_str[0] == '(') {
@@ -97,37 +102,31 @@ void convertToInt() {
   }
 }
 
+/* The below function handles target detection.
+ * It first maps the values from 0 to 1023(current limit) to 0 to 180. This is done due the servo being limited at ~180 degrees.
+ * While it is true that it could instead be done from -90 to 90, doing this does not work, as it limits the servo for some reason into just 90 degrees.
+ * The conditional statement within the function first checks if movement is true. As you remember, movement was the flag within the 
+ * statusCodes function to check if the values being received were the same or were different. If they were different, it would set "movement" to true.
+ * Thus, the below statement checks if this flag is true, then writes on to both servo "x" and "y" the values that were sent over I2C. 
+ * After it has finished writing the values, it sets movement to false to block any further movements.
+ */
 void onDetectTarget() {
   x = map(x, 0, 1023, 0, 180);
   y = map(y, 0, 1023, 0, 180);
   if (movement == true) {
-    for (pos = 0; pos <= x; pos += 1) {
-      myservo.write(pos);
-      delay(15);
-    }
+    myservo.write(x);
+    myservo2.write(y);
+    
+    delay(15);
     movement = false;
-    //pos = 0;
-    //Code to first map the values of xyz into limit of 255, then have the turret move to target and shoot
   }
 }
 
-void compareOldNew() {
-  x_old = x;
-  y_old = y;
-  delay(900);
-  if (x == x_old && y == y_old) {
-    constant = true;
-  } else {
-    constant = false;
-  }
-}
-
+//Functions from above all being run in loop()
 void loop() {
   delay(100);
-  startUp();
   toSend = 100;
   statusCodes();
   convertToInt();
   onDetectTarget();
-  compareOldNew();
 }
