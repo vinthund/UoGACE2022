8b5c6d2126190303fc342d2907de9916b734127c
ad4646d <ad4646d@gre.ac.uk>
2022-03-14 23:20:00 +0000

    [Mod] Created code for 'homing' function based on Pan_Tilt_State_Machine.pdf flow diagram.

--- Pan-TiltAssembly/Arduino_Code/ControlTest/ControlTest.ino
+++ Pan-TiltAssembly/Arduino_Code/ControlTest/ControlTest.ino
@@ -1,34 +1,42 @@
-#include <AccelStepper.h>
+/*
+    Created by: Andrew Dean
+    Description:
+    This code contains the functions for homing the pan/tilt mechanism 
+    and then driving it to target coordinates once it has homed.
+*/
+
+#include <AccelStepper.h>/* ~~ This code requires the AccelStepper library: https://github.com/waspinator/AccelStepper
+                            The library's class references can found here: https://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html#details ~~ */
 #define motorInterfaceType 1
 
-//Stepper pins
+/* ~~ Stepper pins ~~ */
 const int ci_panStepPin = 25;
 const int ci_panDirPin = 24;
 
 const int ci_tiltStepPin = 13;
 const int ci_tiltDirPin = 12;
 
-//Hall effect pins
+/* ~~ Hall effect sensor pins ~~ */
 const int ci_panHomeHallPin = 27;
-//const int ci_panLeftHallPin = 28; // Not used
-//const int ci_panRghtHallPin = 29; // Not used
-
 const int ci_tiltHomeHallPin = 30;
 const int ci_tiltDownHallPin = 32;
 const int ci_tiltUpHallPin = 33;
 
-//Declare objects of AccelStepper to handle pan and tilt axes
+/* ~~ Declare objects of AccelStepper to handle pan and tilt axes ~~ */
 AccelStepper panStepper (motorInterfaceType, ci_panStepPin, ci_panDirPin);
 AccelStepper tiltStepper (motorInterfaceType, ci_tiltStepPin, ci_tiltDirPin);
 
-//Variables
-bool serialDebugPrint = false; //enables serial debug text
-int stateMachine = 0; //Store variable for state machine switch case
+/* ~~ Variables ~~ */
+bool b_serialDebugPrint = false; //enables serial debug text
+bool b_panAxisHomed = false;
+bool b_tiltAxisHomed = false;
+int i_stateMachine = 0; //Store variable for state machine switch case
+int i_tiltHomingDirection; //0 = was previously pointing up, 1 = was previously pointing down
+
+int i_panTargetCoord = 0; //Store the target coordinate for the pan axis
+int i_tiltTargetCoord = 0; //Store the target coordinate for the tilt axis
 
 int i_panHomeHallDetect;
-int i_panLeftHallDetect;
-int i_panRghtHallDetect;
-
 int i_tiltHomeHallDetect;
 int i_tiltDownHallDetect;
 int i_tiltUpHallDetect;
@@ -39,13 +47,13 @@ void setup()
     //Output pins
     pinMode(ci_panStepPin, OUTPUT);
     pinMode(ci_panDirPin, OUTPUT);
+
     pinMode(ci_tiltStepPin, OUTPUT);
     pinMode(ci_tiltDirPin, OUTPUT);
 
     //Input pins
     pinMode(ci_panHomeHallPin, INPUT);
-    //pinMode(ci_panLeftHallPin, INPUT); // Not used
-    //pinMode(ci_panRghtHallPin, INPUT); // Not used
+
     pinMode(ci_tiltHomeHallPin, INPUT);
     pinMode(ci_tiltDownHallPin, INPUT);
     pinMode(ci_tiltUpHallPin, INPUT);
@@ -53,7 +61,7 @@ void setup()
 
 void loop()
 {
-    switch(stateMachine)
+    switch(i_stateMachine)
     {
         case(0):
             homing_func();
@@ -71,8 +79,104 @@ void loop()
 
 void homing_func() //Contains the function for homing the pan/tilt head
 {
+    if(b_tiltAxisHomed = false) // This section will home the tilt axis, it will run first.
+    {
+        tiltStepper.setSpeed(100);
+        tiltStepper.setAcceleration(50);
+        if(i_tiltUpHallDetect == 0)
+        {
+            i_tiltHomingDirection = 0;
+            i_tiltTargetCoord = 133; // If a tilt up/down hall effect is detected, axis is 30˚ from home
+                                     // Tilt stepper  = 1.8˚/step, 4:1 gearing ratio + half stepping = 133 steps to get back home
+            tiltStepper.moveTo(i_tiltTargetCoord); // Drive CW back to approx tilt home hall effect
+            tiltStepper.run();
+            if(b_serialDebugPrint = true)
+            {
+                Serial.println("Tilt axis pointing up, seeking tilt axis home hall effect sensor.");  
+            }
+        }
 
-}
+        if(i_tiltDownHallDetect == 0)
+        {
+            i_tiltHomingDirection = 1;
+            i_tiltTargetCoord = 133; 
+            tiltStepper.moveTo(-i_tiltTargetCoord); // Drive CCW back to approx tilt home hall effect
+            tiltStepper.run();
+            if(b_serialDebugPrint = true)
+            {
+                Serial.println("Tilt axis pointing down, seeking tilt axis home hall effect sensor.");  
+            }
+        }
+
+        if(tiltStepper.distanceToGo() == 0 && i_tiltHomeHallDetect = 1) // if it has walked past the tilt home hall
+        {
+            if(i_tiltHomingDirection == 0)
+            {
+                i_tiltTargetCoord = 1;
+                tiltStepper.moveTo(i_tiltTargetCoord);
+                i_tiltTargetCoord++;
+                tiltStepper.run();
+                if(b_serialDebugPrint == true)
+                {
+                    Serial.println("Tilt axis overshot tilt home hall effect, seeking tilt hall effect sensor.")
+                }
+            }
+            
+            if(i_tiltHomingDirection == 1)
+            {
+                i_tiltTargetCoord = 1;
+                tiltStepper.moveTo(-i_tiltTargetCoord);
+                i_tiltTargetCoord++;
+                tiltStepper.run();
+                if(b_serialDebugPrint == true)
+                {
+                    Serial.println("Tilt axis overshot tilt home hall effect, seeking tilt hall effect sensor.")
+                }
+            }
+        }
+
+        if(i_tiltHomeHallDetect == 0) // If home hall effect is detected
+        {
+            tiltStepper.stop();
+            tiltStepper.setCurrentPosition(0);
+            if(b_serialDebugPrint == true)
+            {
+                Serial.println("Tilt axis homed!")
+            }
+            b_tiltAxisHomed = true;
+        }
+    }// end of tilt homing
+
+    if(b_tiltAxisHomed == true) // Only enter this function once tilt axis has been homed
+    {
+        panStepper.setSpeed(100);
+        panStepper.setAcceleration(50);
+        
+        if(i_panHomeHallDetect == 1) // If the pan hall effect is not detected
+        {
+            i_panTargetCoord = 1;
+            panStepper.moveTo(i_panTargetCoord);
+            i_panTargetCoord++;
+            panStepper.run();
+            if(b_serialDebugPrint == true)
+            {
+                Serial.println("Seeking pan axis home hall effect...");
+            }
+        }
+
+        if(i_panHomeHallDetect == 0)
+        { 
+            panStepper.stop();
+            panStepper.setCurrentPosition(0);
+            if(b_serialDebugPrint == true)
+            {
+                Serial.println("Pan axis homed!")
+            }
+            b_panAxisHomed = true;
+        }
+    }
+
+}// end of homing_func
 
 void scanning_func() //Contains the function for 'scanning' for potential tartets
 {
