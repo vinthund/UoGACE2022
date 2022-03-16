baa030890eef379e6b6b4e1c2563620910e6a466
ad4646d <ad4646d@gre.ac.uk>
2022-03-16 23:42:44 +0000

    [Mod] Added additional comments throughout the code as well as minor housekeeping of the code.

--- FiringMechAssembly/Arduino_Code/Component_Testing/Component_Testing.ino
+++ FiringMechAssembly/Arduino_Code/Component_Testing/Component_Testing.ino
@@ -52,23 +52,20 @@ bool b_panAxisHomed = false;
 bool b_tiltAxisHomed = false;
 int i_stateMachine = 0; //Store variable for state machine switch case
 int i_tiltHomingDirection; //0 = was previously pointing up, 1 = was previously pointing down
+
 int i_panTargetCoord = 0; //Store the target coordinate for the pan axis
 int i_tiltTargetCoord = 0; //Store the target coordinate for the tilt axis
+
 int i_panHomeHallDetect;
-//int i_panLeftHallDetect; // Not used
-//int i_panRghtHallDetect; // Not used
 int i_tiltHomeHallDetect;
 int i_tiltDownHallDetect;
 int i_tiltUpHallDetect;
-
-
 
 //dev
 int i_panScanRange = 200;
 int i_tiltScanRange = 100;
 bool b_panScanDir = 0;
 bool b_tiltScanDir = 0;
-
 
 void setup() {
   //Comms Setup
@@ -99,8 +96,6 @@ void setup() {
 
   //Input pins
   pinMode(ci_panHomeHallPin, INPUT);
-  //pinMode(ci_panLeftHallPin, INPUT); // Not used
-  //pinMode(ci_panRghtHallPin, INPUT); // Not used
   pinMode(ci_tiltHomeHallPin, INPUT);
   pinMode(ci_tiltDownHallPin, INPUT);
   pinMode(ci_tiltUpHallPin, INPUT);
@@ -127,7 +122,7 @@ void loop() {
 
 void home_mag() //Contains the function for homing the magazine to dart zero
 {
-   goToDart(0);
+  goToDart(0);
 }
 
 void goToDart(byte dart) //Contains the function for selecting a dart
@@ -137,14 +132,17 @@ void goToDart(byte dart) //Contains the function for selecting a dart
     delay(30);
     int i_tmp = magSensor.getRawAngle();
     int i_d = ci_dartPos[dart] - i_tmp;
-    if ( i_tmp > (ci_dartPos[dart] - 3) && i_tmp < (ci_dartPos[dart] + 3)) {
+    if ( i_tmp > (ci_dartPos[dart] - 3) && i_tmp < (ci_dartPos[dart] + 3)) 
+    {
       return 0;
     }
-    else if ( i_tmp > ci_dartPos[dart]) {
+    else if ( i_tmp > ci_dartPos[dart]) 
+    {
       magStepper.setSpeed(50); 
       magStepper.runSpeed();
     }
-    else {
+    else 
+    {
       magStepper.setSpeed(-50); 
       magStepper.runSpeed();
     }
@@ -176,9 +174,27 @@ void fire() //Contains the function to fire the selected dart
 
 void homing_func() //Contains the function for homing the pan/tilt head
 {
-  //Tilt Up till Limit
+  /*
+    -- Homing Function --
+    The code in this funciton will drive the pan/tilt head to its 'home' hall effect sensors.
+    
+    - Homing the tilt axis -
+    (1) The tilt axis will first be poiting down as this is more secure for transit
+    (2) Drive tilt axis up at a constant speed until the 'tiltUpHallPin' is detected 'low', then record current coordinate
+    (3) Drive tilt axis down at constant speed until the 'tiltDownHallPin' is detected 'low', then record current coordinates
+    (4) Calculate the midpoint between the coordinates for (2) and (3)
+    (5) Drive tilt axis to calculated midpoint and stop tilt axis
+
+    - Homing the pan axis -
+    (1) Drive the pan axis around at a constant speed until 'panHomehallPin' is detected 'low' 
+    (2) When 'panHomeHallPin' is detected 'low', stop the pan axis and record current coordinate
+
+  */
+
+  //Tilt Up till Limit 
   tiltStepper.setSpeed(-100);
-  while(digitalRead(ci_tiltUpHallPin) == 1){
+  while(digitalRead(ci_tiltUpHallPin) == 1) // Hall effect output is low when magnet is detected
+  {
     tiltStepper.runSpeed();
   }
   tiltStepper.stop();
@@ -187,61 +203,81 @@ void homing_func() //Contains the function for homing the pan/tilt head
 
   //Tilt Down till Limit
   tiltStepper.setSpeed(100);
-  while(digitalRead(ci_tiltDownHallPin) == 1){
+  while(digitalRead(ci_tiltDownHallPin) == 1)
+  {
     tiltStepper.runSpeed();
   }
   tiltStepper.stop();
-  i_tiltDownHallDetect = tiltStepper.currentPosition();
+  i_tiltDownHallDetect = tiltStepper.currentPosition(); 
   delay(200);
 
   //Calculate mid then move to.
-  if (i_tiltDownHallDetect > i_tiltUpHallDetect) {
+  if (i_tiltDownHallDetect > i_tiltUpHallDetect) 
+  {
     i_tiltHomeHallDetect = ((i_tiltDownHallDetect - i_tiltUpHallDetect) / 2) + i_tiltUpHallDetect;
   }
-  else {
+  else 
+  {
     i_tiltHomeHallDetect = ((i_tiltUpHallDetect - i_tiltDownHallDetect) / 2) + i_tiltDownHallDetect;
   }
   tiltStepper.moveTo(i_tiltHomeHallDetect);
-  while(tiltStepper.currentPosition() != i_tiltHomeHallDetect){
+  while(tiltStepper.currentPosition() != i_tiltHomeHallDetect)
+  {
     tiltStepper.run();
   }
   tiltStepper.stop();
+  // Possibly add 'i_tiltHomeHallDetect = tiltStepper.currentPosition(0);' here?
+  // Parsing '0' into 'currentPosition' will set the current location as the Zero point for further coordinates which could be useful.
   delay(200);
 
   //Home Pan
   panStepper.setSpeed(100);
-  while(digitalRead(ci_panHomeHallPin) == 1){
+  while(digitalRead(ci_panHomeHallPin) == 1)
+  {
     panStepper.runSpeed();
   }
   panStepper.stop();
-  i_panHomeHallDetect = panStepper.currentPosition();
+  i_panHomeHallDetect = panStepper.currentPosition(); // If we parse '0' into currentPosition, the current location will be set as coordinate 0
   delay(200);
   
 }// end of homing_func
 
 void scanning_func() //Contains the function for 'scanning' for potential tartets
 {
+  /*
+    -- Scanning Function --
+    The code in this function drives pan and tilt axes between coordinates either side of their home positions.
+    This will allow for the machine vision system to get a better view of potential targets infront of the system.
+
+  */
+
   //Pan Stepper Control
-  if(b_panScanDir == 0){
+  if(b_panScanDir == 0)
+  {
     panStepper.moveTo(i_panHomeHallDetect - i_panScanRange);
   }
-  else{
+  else
+  {
     panStepper.moveTo(i_panHomeHallDetect + i_panScanRange);
   }
 
-  if(panStepper.distanceToGo() == 0){
-    b_panScanDir = !b_panScanDir;
+  if(panStepper.distanceToGo() == 0) // 'distanceToGo' returns how many steps are left until the target value is met
+  {
+    b_panScanDir = !b_panScanDir; // Setting this will cause axis to rotate in opposite direction
   }
     
   //Tilt Stepper Control
-  if(b_tiltScanDir == 0){
+  if(b_tiltScanDir == 0)
+  {
     tiltStepper.moveTo(i_tiltHomeHallDetect - i_tiltScanRange);
   }
-  else{
+  else
+  {
     tiltStepper.moveTo(i_tiltHomeHallDetect + i_tiltScanRange);
   }
 
-  if(tiltStepper.distanceToGo() == 0){
+  if(tiltStepper.distanceToGo() == 0)
+  {
     b_tiltScanDir = !b_tiltScanDir;
   }
 
@@ -251,16 +287,16 @@ void scanning_func() //Contains the function for 'scanning' for potential tartet
 
 void tracking_func() //Contains the function for 'tracking' a target once it has been identified
 {
-    if(Serial.available() > 0)
+  if(Serial.available() > 0)
+  {
+    /* 
+      Add code here that accepts users coordinates
+      Checks to see if inputted coordinates are within bounds of acceptable movement
+      And drives mechanism to user specified coordinates
+    */
+    if(i_tiltTargetCoord > -133 || i_tiltTargetCoord > 133)
     {
-        /* 
-            Add code here that accepts users coordinates
-            Checks to see if inputted coordinates are within bounds of acceptable movement
-            And drives mechanism to user specified coordinates
-        */
-       if(i_tiltTargetCoord > -133 || i_tiltTargetCoord > 133)
-       {
-           Serial.println("Please enter a value between -133 (Look up) and 133 (Look down).");
-       }
+        Serial.println("Please enter a value between -133 (Look up) and 133 (Look down).");
     }
+  }
 } //End of tracking_func
